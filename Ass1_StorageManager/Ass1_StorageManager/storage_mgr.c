82bdeba6db6b20a0c33ee8d3d2477721b62edfcf
Juan Luis Polo <jlpolog  git config --global user.name Your>
2020-01-30 17:43:56 -0600

    pre-commented version

--- Ass1_StorageManager/Ass1_StorageManager/storage_mgr.c
+++ Ass1_StorageManager/Ass1_StorageManager/storage_mgr.c
@@ -33,7 +33,8 @@ extern void initStorageManager (void){
     free(fp);
 }
 
-RC createPageFile (char *fileName){
+
+extern RC createPageFile (char *fileName){
     //Opening the file in read/write mode
     fp = fopen(fileName, "w+");
     if (fp != NULL) {
@@ -67,11 +68,11 @@ extern RC openPageFile (char *fileName, SM_FileHandle *fHandle){
             (*fHandle).totalNumPages = totalNumPages;
             (*fHandle).curPagePos = 0;
             (*fHandle).mgmtInfo = fp;
-//            RC_message = "FILE OPENED CORRECTLY";
+            RC_message = "FILE OPENED CORRECTLY";
             return RC_OK;
         }
         else{
-            RC_message = "RC POINTER NOT SET TO BEGINNING OF FILE";
+            RC_message = "POINTER NOT SET TO BEGINNING OF FILE";
             return RC_READ_NON_EXISTING_PAGE;
         }
     }
@@ -94,7 +95,7 @@ extern RC closePageFile (SM_FileHandle *fHandle){
         }
     }
     else{
-        RC_message = "RC_FILE_HANDLE_NOT_INIT";
+        RC_message = "FILE HANDLE NOT INIT";
         return RC_FILE_HANDLE_NOT_INIT;
     }
 }
@@ -121,56 +122,71 @@ extern RC destroyPageFile (char *fileName){
 //
 
 
-RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
-    if(fHandle->totalNumPages < pageNum)
+extern RC readBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
+    if((*fHandle).totalNumPages < pageNum || pageNum < 0){
+        RC_message = "READ NON EXISTING PAGE";
         return RC_READ_NON_EXISTING_PAGE;
+    }
     else{
-        fp = fopen(fHandle->fileName, "r");
+        fp = fopen((*fHandle).fileName, "r");
         if (fp != NULL) {
             if(fseek(fp, (pageNum*PAGE_SIZE), SEEK_SET) == 0){
                 fread(memPage, sizeof(char), PAGE_SIZE, fp);
-                fclose(fp);
-                return RC_OK;
+                (*fHandle).curPagePos = (int)ftell(fp)/PAGE_SIZE;
+                if(fclose(fp) == 0){
+                    RC_message = "BLOCK CORRECTLY READ";
+                    return RC_OK;
+                }
+                else{
+                    RC_message = "FILE NOT FOUND";
+                    return RC_FILE_NOT_FOUND;
+                }
             }
-            else return RC_READ_NON_EXISTING_PAGE;
+            else{
+                RC_message = "READ NON EXISTING PAGE";
+                return RC_READ_NON_EXISTING_PAGE;
+            }
         }
-        else return RC_FILE_NOT_FOUND;
+        else{
+            RC_message = "FILE NOT FOUND";
+            return RC_FILE_NOT_FOUND;
+        }
     }
 }
 
 
-int getBlockPos (SM_FileHandle *fHandle){
-    return fHandle->curPagePos;
+extern int getBlockPos (SM_FileHandle *fHandle){
+    return (*fHandle).curPagePos;
 }
 
 
-RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+extern RC readFirstBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
     RC firstBlock = readBlock(0, fHandle, memPage);
     return firstBlock;
 }
 
 
-RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-    RC previousBlock = readBlock(fHandle->curPagePos - 1, fHandle, memPage);
+extern RC readPreviousBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    RC previousBlock = readBlock((*fHandle).curPagePos - 1, fHandle, memPage);
     return previousBlock;
 }
 
 
-RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-    RC currentBlock = readBlock(fHandle->curPagePos, fHandle, memPage);
+extern RC readCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    RC currentBlock = readBlock((*fHandle).curPagePos, fHandle, memPage);
     return currentBlock;
 }
 
 
-RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-    RC nextBlock = readBlock(fHandle->curPagePos + 1, fHandle, memPage);
+extern RC readNextBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    RC nextBlock = readBlock((*fHandle).curPagePos + 1, fHandle, memPage);
     return nextBlock;
     
 }
 
 
-RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
-    RC lastBlock = readBlock(fHandle->totalNumPages, fHandle, memPage);
+extern RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+    RC lastBlock = readBlock((*fHandle).totalNumPages, fHandle, memPage);
     return lastBlock;
     
 }
@@ -185,33 +201,45 @@ RC readLastBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
 //
 
 
-RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
-    if(fHandle->totalNumPages < pageNum)
+extern RC writeBlock (int pageNum, SM_FileHandle *fHandle, SM_PageHandle memPage){
+    if((*fHandle).totalNumPages < pageNum || pageNum < 0){
+        RC_message = "WRITE FAILED";
         return RC_WRITE_FAILED;
+    }
     else{
-        fp = fopen(fHandle->fileName, "r+");
+        fp = fopen((*fHandle).fileName, "r+");
         if (fp != NULL) {
             if(fseek(fp, (pageNum*PAGE_SIZE), SEEK_SET) == 0){
-                if(fwrite(memPage, sizeof(char), PAGE_SIZE, fp) != PAGE_SIZE)
-                    return RC_WRITE_FAILED;
-                else{
+                if(fwrite(memPage, sizeof(char), PAGE_SIZE, fp) == PAGE_SIZE){
+                    (*fHandle).curPagePos = (int)ftell(fp)/PAGE_SIZE;
                     fclose(fp);
+                    RC_message = "BLOCK WRITTEN";
                     return RC_OK;
                 }
+                else{
+                    RC_message = "WRITE FAILED";
+                    return RC_WRITE_FAILED;
+                }
             }
-            else return RC_READ_NON_EXISTING_PAGE;
+            else{
+                RC_message = "READ NON EXISTING MESSAGE";
+                return RC_READ_NON_EXISTING_PAGE;
+            }
         }
-        else return RC_FILE_NOT_FOUND;
+        else{
+            RC_message = "FILE NOT FOUND";
+            return RC_FILE_NOT_FOUND;
+        }
     }
 }
 
 
-RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
+extern RC writeCurrentBlock (SM_FileHandle *fHandle, SM_PageHandle memPage){
     return writeBlock((*fHandle).curPagePos, fHandle, memPage);
 }
 
 
-RC appendEmptyBlock (SM_FileHandle *fHandle){
+extern RC appendEmptyBlock (SM_FileHandle *fHandle){
     //calloc() function initializes to zero a dynamic variable type SM_PageHandle
     SM_PageHandle newPage = (SM_PageHandle)calloc(PAGE_SIZE, sizeof(char));
     //Using writeBlock function for appendding the newPage at the end of the file (see writeBlock)
@@ -230,21 +258,22 @@ RC appendEmptyBlock (SM_FileHandle *fHandle){
 }
 
 
-RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){
+extern RC ensureCapacity (int numberOfPages, SM_FileHandle *fHandle){
     
     if((*fHandle).mgmtInfo == NULL){
         RC_message = "RC_FILE_HANDLE_NOT_INIT";
-        return RC_FILE_HANDLE_NOT_INIT;}
+        return RC_FILE_HANDLE_NOT_INIT;
+    }
 
     if((*fHandle).totalNumPages == numberOfPages){
         RC_message = "FILE HAS SAME NUMBER OF PAGES";
-        return RC_OK;}
-    
+        return RC_OK;
+    }
     else{
         RC_message = "FILE HAS LESS NUMBER OF PAGES";
         do{
-            appendEmptyBlock(fHandle);}
-        while(numberOfPages > (*fHandle).totalNumPages);
+            appendEmptyBlock(fHandle);
+        } while(numberOfPages > (*fHandle).totalNumPages);
         
         RC_message = "PAGES HAVE BEEN ADDED AND FILE HAS SAME NUMBER OF PAGES";
         return RC_OK;
