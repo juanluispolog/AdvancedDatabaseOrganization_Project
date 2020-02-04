acd8f7d5527a9ed0008e187de6d1cc481847d9ce
Juan Luis Polo <jlpolog  git config --global user.name Your>
2020-02-04 15:11:21 -0600

    corrected version

---
+++ Ass2_BufferManager/Ass2_BufferManager/buffer_mgr.c
@@ -0,0 +1,10 @@
+//
+//  buffer_mgr.c
+//  Ass2_BufferManager
+//
+//  Created by Juan Luis Polo and Eduardo Moreno on Spring 2020.
+//  Copyright © 2020 Juan Luis Polo and Eduardo Moreno. All rights reserved.
+//
+
+#include <stdio.h>
+#include "buffer_mgr.h"
