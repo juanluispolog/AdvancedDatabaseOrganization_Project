ad00654c36cea1b83df8690edf177e87fae77ddc
Eduardo Moreno <eduardo@MacBook-Pro-de-Eduardo-12377.local>
2020-02-09 17:57:43 -0600

    first function

--- Ass2_BufferManager/Ass2_BufferManager/buffer_mgr.c
+++ Ass2_BufferManager/Ass2_BufferManager/buffer_mgr.c
@@ -8,3 +8,82 @@
 
 #include <stdio.h>
 #include "buffer_mgr.h"
+#include "storage_mgr.h"
+#include "storage_mgr.c"
+
+
+#define PAGE_H
+#include <stdbool.h>
+
+typedef struct Page {
+    //1 means dirty, 0 means clean
+    bool isDirty;
+    bool isPinned;
+    int fixCount;
+    int pageNumber;
+    char *pageValue;
+} Page;
+
+
+// NOTES
+// INIT -> ALL PAGE FRAMES MUST BE EMPTYS
+
+RC initBufferPool(BM_BufferPool *const bm, const char *const pageFileName,
+const int numPages, ReplacementStrategy strategy, void *stratData){
+        
+    SM_FileHandle *fHandle = malloc(1 * sizeof(SM_FileHandle));
+
+    if(openPageFile(pageFileName, fHandle)==RC_OK){
+    
+        //PageFile
+        (*bm).pageFile = pageFileName;
+    
+        //Number of Pages
+        (*bm).numPages = numPages;
+    
+        //Replacement strategy
+        (*bm).strategy = strategy;
+    
+        //Bookeeping info
+        (*bm).mgmtData = fHandle;
+        
+        RC_message = "BUFFER_POOL_INIT";
+        return RC_OK;
+    }
+    
+    RC_message = "BUFFER_POOL_NOT_INIT";
+    return RC_FILE_NOT_FOUND;
+    
+}
+
+RC shutdownBufferPool(BM_BufferPool *const bm){
+    
+    if(forceFlushPool(bm) == RC_OK){
+        
+        
+        
+    }
+    
+    
+    //
+}
+
+RC forceFlushPool(BM_BufferPool *const bm){
+    
+    //(*bm).pageFile
+}
+
+//extern RC openPageFile (char *fileName, SM_FileHandle *fHandle);
+
+
+//typedef struct BM_BufferPool {
+    //char *pageFile;
+    //int numPages;
+    //ReplacementStrategy strategy;
+    //void *mgmtData; // use this one to store the bookkeeping info your buffer
+    // manager needs for a buffer pool
+//} BM_BufferPool;
+
+
+
+
